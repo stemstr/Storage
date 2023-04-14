0f9f979cb0d0fe64dfb36e95c8cfd15eb91c5602
bndw <ben@bdw.to>
2023-04-13 21:19:54 -0700

    feat: mimes package

---
+++ internal/mimes/mimes.go
@@ -0,0 +1,32 @@
+package mimes
+
+import (
+	"strings"
+)
+
+const (
+	AudioAIFF = "audio/aiff"
+	AudioMP3  = "audio/mp3"
+	AudioMP4  = "audio/mp4"
+	AudioOGG  = "audio/ogg"
+	AudioWAV  = "audio/wave"
+)
+
+func FromFilename(name string) string {
+	switch {
+	case strings.HasSuffix(name, ".aif"):
+		return AudioAIFF
+	case strings.HasSuffix(name, ".aiff"):
+		return AudioAIFF
+	case strings.HasSuffix(name, ".mp3"):
+		return AudioMP3
+	case strings.HasSuffix(name, ".m4a"):
+		return AudioMP4
+	case strings.HasSuffix(name, ".ogg"):
+		return AudioOGG
+	case strings.HasSuffix(name, ".wav"):
+		return AudioWAV
+	default:
+		return ""
+	}
+}
---
+++ internal/mimes/mimes_test.go
@@ -0,0 +1,28 @@
+package mimes
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFromFilename(t *testing.T) {
+	var tests = []struct {
+		filename string
+		expected string
+	}{
+		{"test.aif", "audio/aiff"},
+		{"test.mp3", "audio/mp3"},
+		{"test.m4a", "audio/mp4"},
+		{"test.wav", "audio/wave"},
+		{"test.ogg", "audio/ogg"},
+		{"test.wtf", ""},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.filename, func(t *testing.T) {
+			resp := FromFilename(tt.filename)
+			assert.Equal(t, tt.expected, resp)
+		})
+	}
+}
